9d471c6073975922e651584470ffbf540476b9af
noname8383 <59461853+noname8383@users.noreply.github.com>
2020-02-04 13:45:08 +0800

    Create Account.java

---
+++ src/noname/nosshv1/pojo/Account.java
@@ -0,0 +1,40 @@
+package noname.nosshv1.pojo;
+
+public class Account {
+	private int id;
+	private String account;
+	private String username;
+	private String password;
+	private String comment;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+}
